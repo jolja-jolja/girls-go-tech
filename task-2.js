737e21b38d161d59ff487322c62d35c96efccf45
julija_kudrjasova <julia.kudryashova@gmail.com>
2022-07-20 22:09:07 +0300

    100th time

--- task-2.js
+++ task-2.js
@@ -1,3 +1,15 @@
-var Name = "Julija"
+var Name = "Margareth";
+var Cats = 5;
+var true = crazy;
+var Cats = [Tiny, Pokey, Smokey, Finley, Stanley];
+var Pokey ={Cats: "Tiny", favoriteFood: [Whiskas, Cheese], Cats: "pokey", favoriteFood: [Chicken, Cream]};
+var Dogs ="";
+var Husbands = null;
+var Years to live = Name * 5;
+
+
+
+
+
 
 

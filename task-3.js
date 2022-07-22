740648c2d263dd3d3bde7cb6cb2eeec11561e3de
julija_kudrjasova <julia.kudryashova@gmail.com>
2022-07-22 20:40:06 +0300

    js condiditions

---
+++ task-3.js
@@ -0,0 +1,19 @@
+if (10 > 0) {alert ("you have 10")}
+else {alert("You have no money")}
+
+
+switch("cow"){
+    case "cat":
+            alert("meow")
+            break;
+    case "dog":
+        alert("woof")
+        break;
+    case "cow":
+        alert("mooooo")
+        break
+}
+
+2004 === 2004 && "over 18" =="over 18"
+
+12<18 || "minor"===""
